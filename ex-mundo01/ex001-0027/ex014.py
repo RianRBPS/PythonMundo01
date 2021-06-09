9de0c2536d1a9f993876cd61bab3c6296b9d5576
RianRBPS <82774034+RianRBPS@users.noreply.github.com>
2021-06-09 11:47:08 -0300

    'Certificado'

--- ex-mundo01/ex001-0027/ex014.py
+++ ex-mundo01/ex001-0027/ex014.py
@@ -1,6 +1,6 @@
 #Escreva um programa que converta uma temperatura digitando em graus Celsius e converta para graus Fahrenheit
 c = float(input('Informa a temperatura em C: '))
 f = 9*c/5 + 32
-print('A temperatura de {}C corresponde a {}F!'.format(c, f))
+print('A temperatura de {c}C corresponde a {f}F!'.format(c, f))
 
 #f = ((9*c)/5)+32
