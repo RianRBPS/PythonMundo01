cf288914066ba6c9d041e10dd95a4b9685d6c0b3
RianRBPS <82774034+RianRBPS@users.noreply.github.com>
2021-07-12 14:35:02 -0300

    Correção 75

--- B Exercícios/Mundo03 072-/ex075.py
+++ B Exercícios/Mundo03 072-/ex075.py
@@ -4,7 +4,10 @@ numeros = int(input('Digite um número: ')), int(input('Digite mais um número: 
 print(numeros.count(9))
 
 # Em que posição foi digitado o primeiro 3
-print(numeros.index(3))
+if 3 in numeros:
+    print(numeros.index(3))
+else:
+    print('O valor 3 não foi digitado')
 
 # Quais foram os números pares
 print({n for n in numeros if n % 2 == 0})
