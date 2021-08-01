6ae1c1e16f174a649d368fcbae0e54d25dd48f26
RianRBPS <82774034+RianRBPS@users.noreply.github.com>
2021-07-31 23:13:49 -0300

    21 (106) ex ajustes

--- B Exercícios/Mundo03 072-/ex106.py
+++ B Exercícios/Mundo03 072-/ex106.py
@@ -1,8 +1,8 @@
 print('SISTEMA DE AJUDA PyHELP')
 print('-='*30)
 while True:
-    resp = input('Função ou Biblioteca > ')
+    resp = input('\033[0;31mFunção ou Biblioteca >\033[m')
     if resp.upper() == 'FIM':
-        print('Até Logo!')
+        print('\033[0;31mAté Logo!\033[m')
         break
     print(help(resp))
