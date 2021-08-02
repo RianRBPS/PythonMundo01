e764e0bbfb65eecb93f921acf6acefed0d706d2c
RianRBPS <82774034+RianRBPS@users.noreply.github.com>
2021-08-01 21:15:02 -0300

    22 ex 110 \t Dúvida

---
+++ Aulas/Mundo03 - Aulas/Aula22.py
@@ -0,0 +1,8 @@
+#            Modularização
+# Surgiu no início da década de 60
+# Sistemas ficando cada vez maiores
+
+# Foco: Dividir um programa grande
+# Foco: Aumentar a legibilidade
+# Foco: Facilitar a manutenção
+
